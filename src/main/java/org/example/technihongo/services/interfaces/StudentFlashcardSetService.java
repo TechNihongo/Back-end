9474ee8bf3a2c48ad914f26753fa9767e547652b
5th_July <ndao9983@gmail.com>
2025-02-15 01:16:55 +0700

    fix bug and update usermanager

--- src/main/java/org/example/technihongo/services/interfaces/StudentFlashcardSetService.java
+++ src/main/java/org/example/technihongo/services/interfaces/StudentFlashcardSetService.java
@@ -1,12 +1,9 @@
 package org.example.technihongo.services.interfaces;
 
-import org.example.technihongo.dto.FlashcardResponseDTO;
 import org.example.technihongo.dto.FlashcardSetRequestDTO;
 import org.example.technihongo.dto.FlashcardSetResponseDTO;
 
-import java.util.List;
-
-public interface FlashcardSetService {
+public interface StudentFlashcardSetService {
     FlashcardSetResponseDTO createFlashcardSet(Integer studentId, FlashcardSetRequestDTO request);
     FlashcardSetResponseDTO updateFlashcardSet(Integer studentId, Integer flashcardSetId, FlashcardSetRequestDTO request);
     void deleteFlashcardSet(Integer studentId, Integer flashcardSetId);
@@ -14,6 +11,5 @@ public interface FlashcardSetService {
 
     FlashcardSetResponseDTO updateFlashcardSetVisibility(Integer studentId, Integer flashcardSetId, Boolean isPublic);
 
-    // GetAll Flashcard of FlashcardSet
-    List<FlashcardResponseDTO> getAllFlashcardsInSet(Integer flashcardSetId);
+    FlashcardSetResponseDTO getAllFlashcardsInSet(Integer flashcardSetId);
 }
--- src/main/java/org/example/technihongo/services/interfaces/StudentService.java
+++ src/main/java/org/example/technihongo/services/interfaces/StudentService.java
@@ -1,16 +1,13 @@
 package org.example.technihongo.services.interfaces;
 
-import org.example.technihongo.dto.StudentDTO;
 import org.example.technihongo.dto.UpdateProfileDTO;
 import org.example.technihongo.enums.DifficultyLevelEnum;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface StudentService {
-    StudentDTO setDailyGoal(Integer studentId, Integer dailyGoal);
-    StudentDTO updateDifficultyLevel(Integer studentId, DifficultyLevelEnum tag);
+    UpdateProfileDTO setDailyGoal(Integer studentId, Integer dailyGoal);
+    UpdateProfileDTO updateDifficultyLevel(Integer studentId, DifficultyLevelEnum tag);
 
-    void updateUserName(Integer userId, UpdateProfileDTO dto);
-    void updatePassword(Integer userId, UpdateProfileDTO dto);
     @Transactional
     void updateStudentProfile(Integer userId, UpdateProfileDTO dto);
 }
