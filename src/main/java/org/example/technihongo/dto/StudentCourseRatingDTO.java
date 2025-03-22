1e37298578357eee579dc62dc0f88d53f7dbad6b
5th_July <ndao9983@gmail.com>
2025-03-23 00:55:42 +0700

    crud for studentCourseRating & QuizAttempt

---
+++ src/main/java/org/example/technihongo/dto/StudentCourseRatingDTO.java
@@ -0,0 +1,18 @@
+package org.example.technihongo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentCourseRatingDTO {
+    private Integer ratingId;
+    private Integer studentId;
+    private Integer courseId;
+    private Integer rating;
+    private String review;
+}
