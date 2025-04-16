7212eaa57145f1ba4c614e982af4967efd9d81e0
5th_July <ndao9983@gmail.com>
2025-04-17 00:24:48 +0700

    add logic and fix payment

--- src/main/java/org/example/technihongo/repositories/SubscriptionPlanRepository.java
+++ src/main/java/org/example/technihongo/repositories/SubscriptionPlanRepository.java
@@ -2,10 +2,17 @@ package org.example.technihongo.repositories;
 
 import org.example.technihongo.entities.SubscriptionPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Repository
 public interface SubscriptionPlanRepository extends JpaRepository<SubscriptionPlan, Integer> {
     boolean existsByName(String name);
 
+    @Query("SELECT p FROM SubscriptionPlan p WHERE p.price = :price AND p.isActive = true")
+    List<SubscriptionPlan> findByPriceAndIsActiveTrue(@Param("price") BigDecimal price);
 }
